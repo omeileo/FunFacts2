27154e3ea7d073df0d689c11854abdffe32183d3
Jase-Omeileo West <omeileo@icloud.com>
2017-06-29 21:42:39 -0500

    Initial Commit

---
+++ FunFacts2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts2
+//
+//  Created by Jase-Omeileo West on 6/29/17.
+//  Copyright © 2017 Omeileo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
